ebe5bf86d2b98d3e24d9a3648f3658111d155601
EliaPennoni <elia.pennoni1991@gmail.com>
2024-07-23 18:39:53 +0200

    esercizio terminato

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 console.log('js connected')
+
+const startButton = document.querySelector('button');
+startButton.addEventListener('click', function () {
+    console.log('startbutton', startButton, typeof startButton);
+    const griglia = document.getElementById('pratofiorito')
+    for (let i = 1; i <= 100; i++) {
+
+        const newCell = document.createElement('div');
+        newCell.innerHTML = i;
+        newCell.addEventListener('click', function() {
+            newCell.classList.add('cliccato');
+            console.log(i)
+        })
+        griglia.append(newCell);
+    }
+
+
+
+});
